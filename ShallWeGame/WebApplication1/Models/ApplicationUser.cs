8c82cede20ac90a69efbc938ddf29444ff07c19f
Nicolai Reigstad <nicolai887@gmail.com>
2015-11-22 23:06:50 +0100

    ApplicationUser and connection string added

---
+++ ShallWeGame/WebApplication1/Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public Boolean firstLogin { get; set; }
+
+        public ApplicationUser()
+        {
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, String authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            return userIdentity;
+        }
+
+
+
+    }
+}
--- ShallWeGame/WebApplication1/Models/GameContext.cs
+++ ShallWeGame/WebApplication1/Models/GameContext.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace WebApplication1.Models
 {
-    public class GameContext : DbContext
+    public class GameContext : IdentityDbContext<ApplicationUser>
     {
-        
+
+        public GameContext() : base("GameContext")
+        {
+            
+        }
+
+
 
     }
 }
